155c0def4a1569a9ce0b6d70c677b8b174a96c59
Geoff Giller <gillerg8@gmail.com>
2016-11-15 20:57:44 -0500

    Commit Full Width

---
+++ spec/tree-view-full-width-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TreeViewFullWidthView from '../lib/tree-view-full-width-view';
+
+describe('TreeViewFullWidthView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
